970a87a019f5c2084f112266957ad88ae02cb08c
rcohen <rcohen@armosec.io>
2023-02-07 15:17:46 +0200

    getter for event
    Signed-off-by: rcohen <rcohen@armosec.io>

--- pkg/ebpfev/v1/event_interface.go
+++ pkg/ebpfev/v1/event_interface.go
@@ -3,12 +3,23 @@ package ebpfev
 import "time"
 
 type EventData struct {
-	Timestamp   time.Time
-	ContainerID string
-	Ppid        string
-	Pid         string
-	SyscallOp   string
-	SyscallArgs []string
-	Exe         string
-	Cmd         string
+	timestamp   time.Time
+	containerID string
+	ppid        string
+	pid         string
+	syscallOp   string
+	syscallArgs string
+	exe         string
+	cmd         string
+}
+
+type EventClient interface {
+	GetEventTimestamp(ev EventData) time.Time
+	GetEventContainerID(ev EventData) string
+	GetEventPPID(ev EventData) string
+	GetEventPID(ev EventData) string
+	GetEventSyscallOp(ev EventData) string
+	GetEventSyscallArgs(ev EventData) string
+	GetEventEXE(ev EventData) string
+	GetEventCMD(ev EventData) string
 }
