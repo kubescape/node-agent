f6e1d17e9cb9584b35b337485da2632995427129
rcohen <rcohen@armosec.io>
2023-02-13 11:20:15 +0200

    change function name
    Signed-off-by: rcohen <rcohen@armosec.io>

--- pkg/accumulator/accumulator_interface.go
+++ pkg/accumulator/accumulator_interface.go
@@ -1,8 +1,8 @@
 package accumulator
 
 type AcccumulatorClient interface {
-	// this function StartCacheAccumulator will store the data from the ebpf engine
-	StartCacheAccumulator() error
+	// this function StartAccumulator will store the data from the ebpf engine
+	StartAccumulator() error
 	StartContainerAccumulator() error
 	StopContainerAccumulator() error
 }
