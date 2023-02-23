65d24b310c2cac0f638d1a3417da1f738f442bfd
rcohen <rcohen@armosec.io>
2023-02-23 16:46:46 +0200

    add to former commit
    Signed-off-by: rcohen <rcohen@armosec.io>

--- pkg/config/v1/config_data_falco_mock.go
+++ pkg/config/v1/config_data_falco_mock.go
@@ -22,11 +22,11 @@ func (c *ConfigDataFalcoMock) GetFalcoSyscallFilter() []string {
 }
 
 func (c *ConfigDataFalcoMock) GetFalcoKernelObjPath() string {
-	return path.Join(currentDir(), "..", "config", "testdata", "mock_falco_ebpf_engine", "kernel_obj_mock.o")
+	return path.Join(currentDir(), "..", "testdata", "mock_falco_ebpf_engine", "kernel_obj_mock.o")
 }
 
 func (c *ConfigDataFalcoMock) GetEbpfEngineLoaderPath() string {
-	return path.Join(currentDir(), "..", "config", "testdata", "mock_falco_ebpf_engine", "userspace_app_mock")
+	return path.Join(currentDir(), "..", "testdata", "mock_falco_ebpf_engine", "userspace_app_mock")
 }
 
 func currentDir() string {
