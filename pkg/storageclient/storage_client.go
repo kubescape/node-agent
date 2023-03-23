bc38e9088c59679f4c4d035003acbb72c60e637f
rcohen <rcohen@armosec.io>
2023-03-23 09:53:46 +0200

    get SBOM correction
    Signed-off-by: rcohen <rcohen@armosec.io>

--- pkg/storageclient/storage_client.go
+++ pkg/storageclient/storage_client.go
@@ -1,26 +1,43 @@
 package storageclient
 
 import (
-	"context"
+	gcontext "context"
 	"fmt"
 	"os"
+	"time"
 
 	apimachineryerrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 
+	"sniffer/pkg/context"
+
+	"github.com/kubescape/go-logger"
+	"github.com/kubescape/go-logger/helpers"
 	spdxv1beta1 "github.com/kubescape/storage/pkg/apis/softwarecomposition/v1beta1"
 	spdxclient "github.com/kubescape/storage/pkg/generated/clientset/versioned"
 )
 
 const (
-	KubeConfig         = "KUBECONFIG"
-	KubescapeNamespace = "kubescape"
+	KubeConfig                             = "KUBECONFIG"
+	KubescapeNamespace                     = "kubescape"
+	SBOMServerStateExist   SBOMServerState = "Exist"
+	SBOMServerStateDeleted SBOMServerState = "Deleted"
+	retryWatcherSleep                      = 30
 )
+
+type SBOMServerState string
+
+type SBOMMetadata struct {
+	SBOMID string
+	SBOMServerState
+}
 
 type StorageK8SAggregatedAPIClient struct {
 	clientset *spdxclient.Clientset
+	existSBOM map[string]SBOMMetadata
 }
 
 var storageclientErrors map[string]error
@@ -45,13 +62,67 @@ func CreateSBOMStorageK8SAggregatedAPIClient() (*StorageK8SAggregatedAPIClient, 
 	if err != nil {
 		return nil, fmt.Errorf("failed to create K8S Aggregated API Client with err: %v", err)
 	}
-	return &StorageK8SAggregatedAPIClient{
+	storageClient := &StorageK8SAggregatedAPIClient{
 		clientset: clientset,
-	}, nil
+		existSBOM: make(map[string]SBOMMetadata),
+	}
+
+	go storageClient.watchForSBOMs()
+
+	return storageClient, nil
+}
+
+func (sc *StorageK8SAggregatedAPIClient) watchForSBOMs() {
+	for {
+		watcher, err := sc.clientset.SpdxV1beta1().SBOMSPDXv2p3s(KubescapeNamespace).Watch(gcontext.TODO(), metav1.ListOptions{})
+		if err != nil {
+			logger.L().Ctx(context.GetBackgroundContext()).Error("Watch for SBOMs failed ", helpers.Error(err))
+			logger.L().Ctx(context.GetBackgroundContext()).Error("Retry in ", helpers.String(fmt.Sprintf("%d", retryWatcherSleep), " seconds"))
+			time.Sleep(retryWatcherSleep * time.Second)
+			continue
+		}
+		for {
+			event, chanActive := <-watcher.ResultChan()
+			if !chanActive {
+				watcher.Stop()
+				break
+			}
+			if event.Type == watch.Error {
+				watcher.Stop()
+				break
+			}
+
+			SBOM, ok := event.Object.(*spdxv1beta1.SBOMSPDXv2p3)
+			if !ok {
+				continue
+			}
+
+			switch event.Type {
+			case watch.Added:
+				sc.existSBOM[SBOM.Name] = SBOMMetadata{
+					SBOMID:          SBOM.Name,
+					SBOMServerState: SBOMServerStateExist,
+				}
+			case watch.Deleted:
+				sc.existSBOM[SBOM.Name] = SBOMMetadata{
+					SBOMID:          SBOM.Name,
+					SBOMServerState: SBOMServerStateDeleted,
+				}
+			}
+		}
+
+	}
 }
 
 func (sc *StorageK8SAggregatedAPIClient) GetData(key string) (any, error) {
-	SBOM, err := sc.clientset.SpdxV1beta1().SBOMSPDXv2p3s(KubescapeNamespace).Get(context.TODO(), key, metav1.GetOptions{})
+	metadata, exist := sc.existSBOM[key]
+	if !exist {
+		return nil, fmt.Errorf("SBOM not exist in server")
+	}
+	if metadata.SBOMServerState == SBOMServerStateDeleted {
+		return nil, fmt.Errorf("SBOM not exist in server, SBOM deleted")
+	}
+	SBOM, err := sc.clientset.SpdxV1beta1().SBOMSPDXv2p3s(KubescapeNamespace).Get(gcontext.TODO(), key, metav1.GetOptions{})
 	if err != nil {
 		return nil, err
 	}
@@ -62,7 +133,7 @@ func (sc *StorageK8SAggregatedAPIClient) PutData(key string, data any) error {
 	if !ok {
 		return fmt.Errorf("failed to update SBOM: SBOM is not in the right form")
 	}
-	_, err := sc.clientset.SpdxV1beta1().SBOMSPDXv2p3s(KubescapeNamespace).Update(context.TODO(), SBOM, metav1.UpdateOptions{})
+	_, err := sc.clientset.SpdxV1beta1().SBOMSPDXv2p3s(KubescapeNamespace).Update(gcontext.TODO(), SBOM, metav1.UpdateOptions{})
 	if err != nil {
 		return err
 	}
@@ -73,7 +144,7 @@ func (sc *StorageK8SAggregatedAPIClient) PostData(key string, data any) error {
 	if !ok {
 		return fmt.Errorf("failed to update SBOM: SBOM is not in the right form")
 	}
-	retSBOM, err := sc.clientset.SpdxV1beta1().SBOMSPDXv2p3s(KubescapeNamespace).Create(context.TODO(), SBOM, metav1.CreateOptions{})
+	retSBOM, err := sc.clientset.SpdxV1beta1().SBOMSPDXv2p3s(KubescapeNamespace).Create(gcontext.TODO(), SBOM, metav1.CreateOptions{})
 	if err != nil {
 		return err
 	}
