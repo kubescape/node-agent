a502162b698f49856ad034a30d986bccf57c4ea1
David Wertenteil <dwertent@armosec.io>
2023-07-23 12:12:37 +0300

    adding mutex per bucket

--- pkg/filehandler/v1/file_handler_simple.go
+++ pkg/filehandler/v1/file_handler_simple.go
@@ -9,32 +9,59 @@ import (
 
 type SimpleFileHandler struct {
 	mutex sync.RWMutex
-	m     map[string]map[string]bool
+	m     map[string]*sync.RWMutex
+	files map[string]map[string]bool
 }
 
 var _ filehandler.FileHandler = (*SimpleFileHandler)(nil)
 
 func CreateSimpleFileHandler() (*SimpleFileHandler, error) {
 	return &SimpleFileHandler{
-		m: make(map[string]map[string]bool),
+		m:     make(map[string]*sync.RWMutex),
+		files: make(map[string]map[string]bool),
 	}, nil
 }
 
 func (s *SimpleFileHandler) AddFile(ctx context.Context, bucket, file string) error {
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
-	if _, ok := s.m[bucket]; !ok {
-		s.m[bucket] = make(map[string]bool)
+	// Acquire a read lock first
+	s.mutex.RLock()
+	bucketLock, ok := s.m[bucket]
+	bucketFiles, okF := s.files[bucket]
+	s.mutex.RUnlock()
+
+	// If the bucket doesn't exist, acquire a write lock to create the new bucket
+	if !ok || !okF {
+		s.mutex.Lock()
+
+		// Double-check the bucket's existence to ensure another goroutine didn't already create it
+		bucketLock, ok = s.m[bucket]
+		if !ok {
+			bucketLock = &sync.RWMutex{}
+			s.m[bucket] = bucketLock
+		}
+
+		bucketFiles, okF = s.files[bucket]
+		if !okF {
+			bucketFiles = make(map[string]bool)
+			s.files[bucket] = bucketFiles
+		}
+
 	}
-	s.m[bucket][file] = true
+
+	// Acquire a write lock if the bucket already exists
+	bucketLock.Lock()
+	defer bucketLock.Unlock()
+
+	bucketFiles[file] = true
+
 	return nil
 }
 
 func (s *SimpleFileHandler) Close() {
+	// Nothing to do
 }
 
-// deepcopy map[string]bool
-func shalowCopyMapStringBool(m map[string]bool) map[string]bool {
+func shallowCopyMapStringBool(m map[string]bool) map[string]bool {
 	if m == nil {
 		return nil
 	}
@@ -45,18 +72,27 @@ func shalowCopyMapStringBool(m map[string]bool) map[string]bool {
 	return mCopy
 }
 
-func (s *SimpleFileHandler) GetFiles(ctx context.Context, container string) (map[string]bool, error) {
+func (s *SimpleFileHandler) GetFiles(ctx context.Context, bucket string) (map[string]bool, error) {
 	s.mutex.RLock()
-	defer s.mutex.RUnlock()
-	if c, ok := s.m[container]; ok {
-		return shalowCopyMapStringBool(c), nil
+	bucketLock, ok := s.m[bucket]
+	bucketFiles, okFiles := s.files[bucket]
+	s.mutex.RUnlock()
+
+	if !ok || !okFiles {
+		return map[string]bool{}, fmt.Errorf("bucket does not exist for container %s", bucket)
 	}
-	return map[string]bool{}, fmt.Errorf("bucket does not exist for container %s", container)
+
+	bucketLock.RLock()
+	defer bucketLock.RUnlock()
+
+	return shallowCopyMapStringBool(bucketFiles), nil
 }
 
 func (s *SimpleFileHandler) RemoveBucket(ctx context.Context, bucket string) error {
 	s.mutex.Lock()
-	defer s.mutex.Unlock()
 	delete(s.m, bucket)
+	delete(s.files, bucket)
+	s.mutex.Unlock()
+
 	return nil
 }
