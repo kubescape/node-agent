3916d3ad8ae5f70deb704990beb13d90be604b6b
David Wertenteil <dwertent@armosec.io>
2023-07-25 10:38:10 +0300

    reduce map init size
    Signed-off-by: David Wertenteil <dwertent@armosec.io>

--- pkg/filehandler/v1/inmemory.go
+++ pkg/filehandler/v1/inmemory.go
@@ -6,8 +6,8 @@ import (
 	"sync"
 )
 
-const initFileListLength = 5000
-const updateFileListLength = 200
+const initFileListLength = 500
+const updateFileListLength = 20
 
 type filesBucket struct {
 	lock  *sync.RWMutex
@@ -23,7 +23,7 @@ var _ filehandler.FileHandler = (*InMemoryFileHandler)(nil)
 
 func CreateInMemoryFileHandler() (*InMemoryFileHandler, error) {
 	return &InMemoryFileHandler{
-		buckets: make(map[string]*filesBucket, 20),
+		buckets: make(map[string]*filesBucket, 10),
 	}, nil
 }
 
