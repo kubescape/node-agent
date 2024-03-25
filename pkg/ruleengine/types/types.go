755ae15c8ba4bdd8bd8174b6a071be159529d12d
Amit Schendel <amitschendel@gmail.com>
2024-03-25 14:01:21 +0200

    Adding syscall event type
    Signed-off-by: Amit Schendel <amitschendel@gmail.com>

--- pkg/ruleengine/types/types.go
+++ pkg/ruleengine/types/types.go
@@ -5,8 +5,13 @@ import (
 )
 
 type SyscallEvent struct {
-	eventtypes.BasicK8sMetadata
+	eventtypes.Event
+	eventtypes.WithMountNsID
 
-	// Syscall event specific fields
-	SyscallName string
+	Pid  uint32 `json:"pid,omitempty" column:"pid,template:pid"`
+	Comm string `json:"comm,omitempty" column:"comm,template:comm"`
+	Uid  uint32 `json:"uid" column:"uid,template:uid,hide"`
+	Gid  uint32 `json:"gid" column:"gid,template:gid,hide"`
+
+	SyscallName string `json:"syscallName,omitempty" column:"syscallName"`
 }
