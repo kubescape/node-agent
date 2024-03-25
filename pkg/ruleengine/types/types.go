c0d501c4a367a3c0974c9bb0e25e26ea8f4a4d11
Amit Schendel <amitschendel@gmail.com>
2024-03-25 11:13:16 +0200

    Adding syscall type
    Signed-off-by: Amit Schendel <amitschendel@gmail.com>

--- pkg/ruleengine/types/types.go
+++ pkg/ruleengine/types/types.go
@@ -1,1 +1,12 @@
 package types
+
+import (
+	eventtypes "github.com/inspektor-gadget/inspektor-gadget/pkg/types"
+)
+
+type SyscallEvent struct {
+	eventtypes.BasicK8sMetadata
+
+	// Syscall event specific fields
+	SyscallName string
+}
