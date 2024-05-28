dc6261e61fa21aa47f69aad628a60cb1a0fc6fb6
Amit Schendel <amitschendel@gmail.com>
2024-05-28 08:57:23 +0000

    Adding better tests for ebpf program loading
    Signed-off-by: Amit Schendel <amitschendel@gmail.com>

--- pkg/ruleengine/v1/r0009_ebpf_program_load_test.go
+++ pkg/ruleengine/v1/r0009_ebpf_program_load_test.go
@@ -29,13 +29,42 @@ func TestR0009EbpfProgramLoad(t *testing.T) {
 		return
 	}
 
+	// Create a new rule
+	r2 := CreateRuleR0009EbpfProgramLoad()
+	// Assert r is not nil
+	if r2 == nil {
+		t.Errorf("Expected r to not be nil")
+	}
+
 	// Create a syscall event with bpf syscall
 	e.Syscall = "bpf"
+	e.Parameters = []tracersyscallstype.SyscallParam{
+		{
+			Name:  "cmd",
+			Value: "5", // BPF_PROG_LOAD
+		},
+	}
 
-	ruleResult = r.ProcessEvent(utils.SyscallEventType, e, &RuleObjectCacheMock{})
+	ruleResult = r2.ProcessEvent(utils.SyscallEventType, e, &RuleObjectCacheMock{})
 	if ruleResult == nil {
 		fmt.Printf("ruleResult: %v\n", ruleResult)
 		t.Errorf("Expected ruleResult to be Failure because of bpf is used")
+		return
+	}
+
+	// Create a new rule
+	r3 := CreateRuleR0009EbpfProgramLoad()
+	// Assert r is not nil
+	if r3 == nil {
+		t.Errorf("Expected r to not be nil")
+	}
+
+	// Create a syscall event with bpf syscall but not BPF_PROG_LOAD
+	e.Parameters[0].Value = "1"
+	ruleResult = r3.ProcessEvent(utils.SyscallEventType, e, &RuleObjectCacheMock{})
+	if ruleResult != nil {
+		fmt.Printf("ruleResult: %v\n", ruleResult)
+		t.Errorf("Expected ruleResult to be nil since syscall is bpf but not BPF_PROG_LOAD")
 		return
 	}
 }
