5b2e8d1d36cb2af845015a8ee574840291a1a967
Afek Berger <afekb@armosec.io>
2024-09-18 13:23:43 +0300

    WIP: Modified struct

--- pkg/ebpf/gadgets/ptrace/tracer/bpf/ptrace_detector.c
+++ pkg/ebpf/gadgets/ptrace/tracer/bpf/ptrace_detector.c
@@ -5,18 +5,24 @@
 #include <linux/filter.h>
 
 struct syscalls_enter_ptrace_args {
-    __u64 __unused_syscall_nr;
-    long request;
-    long pid;
-    unsigned long addr;
-    unsigned long data;
-};
+    __u16 common_type;
+    __u8 common_flags;
+    __u8 common_preempt_count;
+    __s32 common_pid;
+
+    __s32 __syscall_nr;
+    __u64 request;
+    __u64 pid;
+    __u64 addr;
+    __u64 data;
+};  
 
 SEC("tracepoint/syscalls/sys_enter_ptrace")
 int trace_enter_ptrace(struct syscalls_enter_ptrace_args *ctx)
 {
     long request = ctx->request;
     long pid = ctx->pid;
+    bpf_printk("Hello from eBPF program: request=%ld, pid=%d\n", request, pid);
 
     if (request == PTRACE_SETREGS || request == PTRACE_POKETEXT || request == PTRACE_POKEDATA) {
         bpf_printk("Malicious ptrace detected: request=%ld, pid=%d\n", request, pid);
