8bd23f4fac36dc43ecba17275df37d854a80a6b6
Afek Berger <afekb@armosec.io>
2024-09-25 10:22:50 +0300

    unique by pid and not tid

--- pkg/ebpf/gadgets/http/tracer/bpf/http-sniffer.c
+++ pkg/ebpf/gadgets/http/tracer/bpf/http-sniffer.c
@@ -82,8 +82,8 @@ static __always_inline int should_discard()
 
 static __always_inline __u64 generate_unique_connection_id(__u64 pid_tgid, __u32 sockfd)
 {
-    __u32 tgid = pid_tgid >> 32; // Correctly extract TGID from upper 32 bits
-    return ((__u64)tgid << 32) | sockfd;
+    __u32 pid = pid_tgid & 0xFFFFFFFF;  // PID is in the lower 32 bits
+    return ((__u64)pid << 32) | sockfd;
 }
 
 static __always_inline void get_namespace_ids(u64 *mnt_ns_id)
