75fd57b87a3d6a17c897ca84515192ca05c59c32
Afek Berger <afekb@armosec.io>
2024-09-29 11:10:13 +0300

    added tracer

---
+++ pkg/ebpf/gadgets/ptrace/tracer/tracer.go
@@ -0,0 +1,186 @@
+package tracer
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/link"
+	"github.com/cilium/ebpf/perf"
+	gadgetcontext "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-context"
+	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets"
+	eventtypes "github.com/inspektor-gadget/inspektor-gadget/pkg/types"
+	"github.com/kubescape/go-logger"
+	"github.com/kubescape/node-agent/pkg/ebpf/gadgets/lib/tracepoint/tracepointlib"
+	"github.com/kubescape/node-agent/pkg/ebpf/gadgets/ptrace/types"
+)
+
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -strip /usr/bin/llvm-strip-18 -no-global-types -target bpfel -cc clang -cflags "-g -O2 -Wall -D __TARGET_ARCH_x86" -type event ptrace bpf/ptrace_detector.c -- -I./bpf/
+const (
+	EVENT_TYPE_CONNECT = iota
+	EVENT_TYPE_ACCEPT
+	EVENT_TYPE_REQUEST
+	EVENT_TYPE_RESPONSE
+	EVENT_TYPE_CLOSE
+)
+
+type Config struct {
+	MountnsMap *ebpf.Map
+}
+
+type Tracer struct {
+	config        *Config
+	enricher      gadgets.DataEnricherByMntNs
+	eventCallback func(*types.Event)
+
+	objs ptraceObjects
+
+	ptracelinks []link.Link
+	reader      *perf.Reader
+}
+
+func NewTracer(config *Config, enricher gadgets.DataEnricherByMntNs,
+	eventCallback func(*types.Event),
+) (*Tracer, error) {
+	t := &Tracer{
+		config:        config,
+		enricher:      enricher,
+		eventCallback: eventCallback,
+	}
+
+	if err := t.install(); err != nil {
+		t.Close()
+		return nil, err
+	}
+
+	go t.run()
+
+	return t, nil
+}
+
+func (t *Tracer) Close() {
+	for _, l := range t.ptracelinks {
+		gadgets.CloseLink(l)
+	}
+
+	if t.reader != nil {
+		t.reader.Close()
+	}
+
+	t.objs.Close()
+
+}
+
+func (t *Tracer) install() error {
+	var err error
+	spec, err := loadPtrace()
+	if err != nil {
+		return fmt.Errorf("loading ebpf program: %w", err)
+	}
+
+	if err := gadgets.LoadeBPFSpec(t.config.MountnsMap, spec, nil, &t.objs); err != nil {
+		return fmt.Errorf("loading ebpf spec: %w", err)
+	}
+
+	tracepoints := tracepointlib.TracepointInfo{"ptrace_enter": t.objs.ptracePrograms.TraceEnterPtrace}
+	var links []link.Link
+	for _, tp := range tracepoints {
+		l, err := tracepointlib.AttachTracepoint(tp)
+		if err != nil {
+			logger.L().Error(fmt.Sprintf("Error attaching tracepoint: %s", err))
+		}
+		links = append(links, l)
+	}
+
+	t.ptracelinks = links
+
+	t.reader, err = perf.NewReader(t.objs.ptraceMaps.Events, gadgets.PerfBufferPages*os.Getpagesize())
+	if err != nil {
+		return fmt.Errorf("creating perf ring buffer: %w", err)
+	}
+
+	return nil
+}
+
+func (t *Tracer) run() {
+	for {
+		record, err := t.reader.Read()
+		if err != nil {
+			if errors.Is(err, perf.ErrClosed) {
+				// nothing to do, we're done
+				continue
+			}
+			msg := fmt.Sprintf("Error reading perf ring buffer: %s", err)
+			t.eventCallback(types.Base(eventtypes.Err(msg)))
+			continue
+		}
+
+		if record.LostSamples > 0 {
+			msg := fmt.Sprintf("lost %d samples", record.LostSamples)
+			t.eventCallback(types.Base(eventtypes.Warn(msg)))
+			continue
+		}
+
+		bpfEvent := tracepointlib.ConvertToEvent[ptraceEvent](&record)
+		event := t.parseEvent(bpfEvent)
+		t.eventCallback(&event)
+	}
+}
+
+func (t *Tracer) Run(gadgetCtx gadgets.GadgetContext) error {
+	defer t.Close()
+	if err := t.install(); err != nil {
+		return fmt.Errorf("installing tracer: %w", err)
+	}
+
+	go t.run()
+	gadgetcontext.WaitForTimeoutOrDone(gadgetCtx)
+
+	return nil
+}
+
+func (t *Tracer) SetMountNsMap(mountnsMap *ebpf.Map) {
+	t.config.MountnsMap = mountnsMap
+}
+
+func (t *Tracer) SetEventHandler(handler any) {
+	nh, ok := handler.(func(ev *types.Event))
+	if !ok {
+		panic("event handler invalid")
+	}
+	t.eventCallback = nh
+}
+
+func (t *Tracer) parseEvent(bpfEvent *ptraceEvent) *types.Event {
+
+	event := types.Event{
+		Event: eventtypes.Event{
+			Type:      eventtypes.NORMAL,
+			Timestamp: gadgets.WallTimeFromBootTime(bpfEvent.Timestamp),
+		},
+		WithMountNsID: eventtypes.WithMountNsID{MountNsID: bpfEvent.MntnsId},
+		Pid:           bpfEvent.Pid,
+		PPid:          bpfEvent.Ppid,
+		Uid:           bpfEvent.Uid,
+		Gid:           bpfEvent.Gid,
+		Request:       bpfEvent.Request,
+		Comm:          gadgets.FromCString(bpfEvent.Comm[:]),
+		ExePath:       gadgets.FromCString(bpfEvent.Exepath[:]),
+	}
+
+	if t.enricher != nil {
+		t.enricher.EnrichByMntNs(&event.CommonData, event.MountNsID)
+	}
+
+	return &event
+}
+
+type GadgetDesc struct{}
+
+func (g *GadgetDesc) NewInstance() (gadgets.Gadget, error) {
+	tracer := &Tracer{
+		config: &Config{},
+	}
+	return tracer, nil
+}
---
+++ pkg/ebpf/gadgets/ptrace/tracer/types/types.go
@@ -0,0 +1,15 @@
+package types
+
+import eventtypes "github.com/inspektor-gadget/inspektor-gadget/pkg/types"
+
+type Event struct {
+	eventtypes.Event
+	eventtypes.WithMountNsID
+	Pid     uint32 `json:"pid,omitempty" column:"pid,template:pid"`
+	PPid    uint32 `json:"ppid,omitempty" column:"ppid,template:ppid"`
+	Uid     uint32 `json:"uid,omitempty" column:"uid,template:uid"`
+	Gid     uint32 `json:"gid,omitempty" column:"gid,template:gid"`
+	Request uint32 `json:"request,omitempty" column:"request,template:request"`
+	Comm    string `json:"comm,omitempty" column:"comm,template:comm"`
+	ExePath string `json:"exe_path,omitempty" column:"exe_path,template:exe_path"`
+}
