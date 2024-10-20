6b0f5614d1274e874a5910f115baf814006d6f7d
Afek Berger <afekb@armosec.io>
2024-10-20 11:33:17 +0300

    WIP: Added enrich type

--- pkg/utils/events.go
+++ pkg/utils/events.go
@@ -1,8 +1,15 @@
 package utils
 
+import "github.com/inspektor-gadget/inspektor-gadget/pkg/types"
+
 type K8sEvent interface {
 	GetPod() string
 	GetNamespace() string
+}
+
+type EnrichEvent interface {
+	types.Event
+	GetPID() int
 }
 
 type EventType string
