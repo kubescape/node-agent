27e663091502dd524ed22e52af933d5c8cf69003
Afek Berger <afekb@armosec.io>
2025-02-03 11:09:47 +0200

    WIP: Added ioring loader
    Signed-off-by: Afek Berger <afekb@armosec.io>

--- pkg/ebpf/gadgets/iouring/tracer/tracer.go
+++ pkg/ebpf/gadgets/iouring/tracer/tracer.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"unsafe"
 
 	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/link"
@@ -11,6 +12,8 @@ import (
 	gadgetcontext "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-context"
 	"github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets"
 	eventtypes "github.com/inspektor-gadget/inspektor-gadget/pkg/types"
+	"github.com/kubescape/node-agent/pkg/ebpf/gadgets/iouring/tracer/types"
+	tracepointlib "github.com/kubescape/node-agent/pkg/ebpf/lib"
 )
 
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go -strip /usr/bin/llvm-strip-18 -no-global-types -target bpfel -cc clang -cflags "-g -O2 -Wall -D __TARGET_ARCH_x86" -type event iouring bpf/iouring.c -- -I./bpf/
@@ -19,29 +22,17 @@ type Config struct {
 	MountnsMap *ebpf.Map
 }
 
-type Event struct {
-	eventtypes.Event
-	Opcode   uint32
-	Pid      uint32
-	Tid      uint32
-	Uid      uint32
-	Gid      uint32
-	Comm     string
-	Flags    uint32
-	UserData uint64
-}
-
 type Tracer struct {
 	config        *Config
 	enricher      gadgets.DataEnricherByMntNs
-	eventCallback func(*Event)
+	eventCallback func(*types.Event)
 	objs          iouringObjects
 	links         []link.Link
 	reader        *perf.Reader
 }
 
 func NewTracer(config *Config, enricher gadgets.DataEnricherByMntNs,
-	eventCallback func(*Event)) (*Tracer, error) {
+	eventCallback func(*types.Event)) (*Tracer, error) {
 	t := &Tracer{
 		config:        config,
 		enricher:      enricher,
@@ -77,7 +68,7 @@ func (t *Tracer) install() error {
 		return fmt.Errorf("loading ebpf spec: %w", err)
 	}
 
-	tracepoint, err := link.Tracepoint("io_uring", "io_uring_submit_sqe", t.objs.TraceIoUringSubmit)
+	tracepoint, err := link.Tracepoint("io_uring", "io_uring_submit_sqe", t.objs.TraceIoUringSubmit, nil)
 	if err != nil {
 		return fmt.Errorf("attaching tracepoint: %w", err)
 	}
@@ -99,24 +90,18 @@ func (t *Tracer) run() {
 				return
 			}
 			msg := fmt.Sprintf("Error reading perf ring buffer: %s", err)
-			t.eventCallback(&Event{Event: eventtypes.Err(msg)})
+			t.eventCallback(&types.Event{Event: eventtypes.Err(msg)})
 			continue
 		}
 
 		if record.LostSamples > 0 {
 			msg := fmt.Sprintf("lost %d samples", record.LostSamples)
-			t.eventCallback(&Event{Event: eventtypes.Warn(msg)})
+			t.eventCallback(&types.Event{Event: eventtypes.Warn(msg)})
 			continue
 		}
 
-		var event iouringEvent
-		if err := event.Unmarshal(record.RawSample); err != nil {
-			msg := fmt.Sprintf("error parsing event: %s", err)
-			t.eventCallback(&Event{Event: eventtypes.Err(msg)})
-			continue
-		}
-
-		t.eventCallback(t.parseEvent(&event))
+		bpfEvent := tracepointlib.ConvertToEvent[iouringEvent](&record)
+		t.eventCallback(t.parseEvent(bpfEvent))
 	}
 }
 
@@ -137,27 +122,28 @@ func (t *Tracer) SetMountNsMap(mountnsMap *ebpf.Map) {
 }
 
 func (t *Tracer) SetEventHandler(handler any) {
-	nh, ok := handler.(func(ev *Event))
+	nh, ok := handler.(func(ev *types.Event))
 	if !ok {
 		panic("invalid event handler type")
 	}
 	t.eventCallback = nh
 }
 
-func (t *Tracer) parseEvent(bpfEvent *iouringEvent) *Event {
-	return &Event{
+func (t *Tracer) parseEvent(bpfEvent *iouringEvent) *types.Event {
+	return &types.Event{
 		Event: eventtypes.Event{
 			Type:      eventtypes.NORMAL,
 			Timestamp: gadgets.WallTimeFromBootTime(bpfEvent.Timestamp),
 		},
-		Opcode:   bpfEvent.Opcode,
-		Pid:      bpfEvent.Pid,
-		Tid:      bpfEvent.Tid,
-		Uid:      bpfEvent.Uid,
-		Gid:      bpfEvent.Gid,
-		Comm:     gadgets.FromCString(bpfEvent.Comm[:]),
-		Flags:    bpfEvent.Flags,
-		UserData: bpfEvent.UserData,
+		WithMountNsID: eventtypes.WithMountNsID{MountNsID: bpfEvent.MntnsId},
+		Opcode:        bpfEvent.Opcode,
+		Pid:           bpfEvent.Pid,
+		Tid:           bpfEvent.Tid,
+		Uid:           bpfEvent.Uid,
+		Gid:           bpfEvent.Gid,
+		Comm:          gadgets.FromCString(*(*[]byte)(unsafe.Pointer(&bpfEvent.Comm))),
+		Flags:         bpfEvent.Flags,
+		UserData:      bpfEvent.UserData,
 	}
 }
 
