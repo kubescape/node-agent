364bb08c9ed2a4230a9c6e0e71f4be800e44ad6c
Amit Schendel <amitschendel@gmail.com>
2025-02-16 18:09:18 +0000

    Adding path and rules
    Signed-off-by: Amit Schendel <amitschendel@gmail.com>

--- Makefile
+++ Makefile
@@ -6,10 +6,10 @@ TAG?=test
 # TAG?=v0.0.1
 
 binary:
-	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o $(BINARY_NAME)
-
-prod-binary:
 	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o $(BINARY_NAME) -ldflags "-s -w"
+
+debug-binary:
+	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o $(BINARY_NAME)
 
 docker-build:
 	docker buildx build --platform linux/amd64 -t $(IMAGE):$(TAG) -f $(DOCKERFILE_PATH) --load .
--- pkg/hostrules/v1/factory.go
+++ pkg/hostrules/v1/factory.go
@@ -8,12 +8,18 @@ import (
 func NewRuleCreator() *ruleenginev1.RuleCreatorImpl {
 	return &ruleenginev1.RuleCreatorImpl{
 		RuleDescriptions: []ruleengine.RuleDescriptor{
+			ruleenginev1.R0008ReadEnvironmentVariablesProcFSRuleDescriptor,
+			ruleenginev1.R0010UnexpectedSensitiveFileAccessRuleDescriptor,
+			ruleenginev1.R1003MaliciousSSHConnectionRuleDescriptor,
+			ruleenginev1.R1010SymlinkCreatedOverSensitiveFileRuleDescriptor,
+			ruleenginev1.R1012HardlinkCreatedOverSensitiveFileRuleDescriptor,
 			ruleenginev1.R1000ExecFromMaliciousSourceDescriptor,
 			ruleenginev1.R1002LoadKernelModuleRuleDescriptor,
 			ruleenginev1.R1005FilelessExecutionRuleDescriptor,
 			ruleenginev1.R1007XMRCryptoMiningRuleDescriptor,
 			ruleenginev1.R1008CryptoMiningDomainCommunicationRuleDescriptor,
 			ruleenginev1.R1015MaliciousPtraceUsageRuleDescriptor,
+			ruleenginev1.R1030UnexpectedIouringOperationRuleDescriptor,
 		},
 	}
 }
