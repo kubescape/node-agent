2b492f076ec862d1c25040017bdc5ee021ef2b02
Amit Schendel <amitschendel@gmail.com>
2025-02-16 11:51:46 +0000

    Adding mock
    Signed-off-by: Amit Schendel <amitschendel@gmail.com>

--- pkg/hostrulemanager/hostrulemanager_mock.go
+++ pkg/hostrulemanager/hostrulemanager_mock.go
@@ -1,1 +1,16 @@
 package hostrulemanager
+
+import "github.com/kubescape/node-agent/pkg/utils"
+
+type HostRuleManagerMock struct {
+}
+
+var _ HostRuleManagerClient = (*HostRuleManagerMock)(nil)
+
+func CreateHostRuleManagerMock() *HostRuleManagerMock {
+	return &HostRuleManagerMock{}
+}
+
+func (r *HostRuleManagerMock) ReportEvent(_ utils.EventType, _ utils.K8sEvent) {
+	// noop
+}
