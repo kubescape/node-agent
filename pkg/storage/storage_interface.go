d758c74dbfa1c74a5c715b1900bd3caa645b64f7
Matthias Bertschy <matthias.bertschy@gmail.com>
2025-04-14 13:48:00 +0200

    remove deprecated types
    Signed-off-by: Matthias Bertschy <matthias.bertschy@gmail.com>

--- pkg/storage/storage_interface.go
+++ pkg/storage/storage_interface.go
@@ -6,7 +6,6 @@ import (
 )
 
 type StorageClient interface {
-	CreateApplicationActivity(activity *v1beta1.ApplicationActivity, namespace string) error
 	CreateApplicationProfile(profile *v1beta1.ApplicationProfile, namespace string) error
 	PatchApplicationProfile(name, namespace string, operations []utils.PatchOperation, channel chan error) error
 	GetApplicationProfile(namespace, name string) (*v1beta1.ApplicationProfile, error)
@@ -16,10 +15,6 @@ type StorageClient interface {
 	ReplaceSBOM(SBOM *v1beta1.SBOMSyft) (*v1beta1.SBOMSyft, error)
 	IncrementImageUse(imageID string)
 	DecrementImageUse(imageID string)
-	GetNetworkNeighbors(namespace, name string) (*v1beta1.NetworkNeighbors, error)
-	CreateNetworkNeighbors(networkNeighbors *v1beta1.NetworkNeighbors, namespace string) error
-	PatchNetworkNeighborsMatchLabels(name, namespace string, networkNeighbors *v1beta1.NetworkNeighbors) error
-	PatchNetworkNeighborsIngressAndEgress(name, namespace string, networkNeighbors *v1beta1.NetworkNeighbors) error
 	GetNetworkNeighborhood(namespace, name string) (*v1beta1.NetworkNeighborhood, error)
 	CreateNetworkNeighborhood(neighborhood *v1beta1.NetworkNeighborhood, namespace string) error
 	PatchNetworkNeighborhood(name, namespace string, operations []utils.PatchOperation, channel chan error) error
