466d6097a5b014f2d4b6eeed9f661f41379e80a4
Afek <afekb@armosec.io>
2025-06-25 12:44:59 +0700

    added proc fs feeder
    Signed-off-by: Afek <afekb@armosec.io>

--- pkg/processtree/feeder/feeder_interface.go
+++ pkg/processtree/feeder/feeder_interface.go
@@ -14,6 +14,12 @@ const (
 	ProcfsEvent
 )
 
+type ProcessEventFeeder interface {
+	Start(ctx context.Context) error
+	Stop() error
+	Subscribe(chan<- ProcessEvent)
+}
+
 type ProcessEvent struct {
 	Type      ProcessEventType
 	Timestamp time.Time
@@ -35,14 +41,4 @@ type ProcessEvent struct {
 	// Host context
 	HostPID  int
 	HostPPID int
-
-	// Extra fields for advanced correlation (optional)
-	ProcessEntityId uint32 // unique process hash/entity id
-	ParentEntityId  uint32 // unique parent process hash/entity id
-}
-
-type ProcessEventFeeder interface {
-	Start(ctx context.Context) error
-	Stop() error
-	Subscribe(chan<- ProcessEvent)
 }
