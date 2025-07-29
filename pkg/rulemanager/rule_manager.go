d92f89c533cb7c487a6668d2adac8d4b95667657
Afek Berger <afekb@armosec.io>
2025-07-29 10:40:53 +0300

    separated mock
    Signed-off-by: Afek Berger <afekb@armosec.io>

--- pkg/rulemanager/rule_manager.go
+++ pkg/rulemanager/rule_manager.go
@@ -225,7 +225,7 @@ func (rm *RuleManager) getProfileChecks(enrichedEvent *events.EnrichedEvent) map
 
 	sharedData := rm.objectCache.K8sObjectCache().GetSharedContainerData(enrichedEvent.ContainerID)
 	if sharedData != nil {
-		containerName := sharedData.ContainerInfos[objectcache.ContainerType(1)][0].Name
+		containerName := sharedData.ContainerInfos[objectcache.ContainerType(sharedData.ContainerType)][0].Name
 
 		ap, nn, ok := rm.registry.GetAvailableProfiles(containerName, enrichedEvent.ContainerID)
 		if ok {
