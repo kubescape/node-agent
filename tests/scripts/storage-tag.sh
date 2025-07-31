f3bc83953923ac9d5de5ed0cde03b5937b71cce0
Matthias Bertschy <matthias.bertschy@gmail.com>
2025-07-31 16:07:04 +0200

    solve TOC/TOU in caches, containerwatcher and malwaremanager
    Signed-off-by: Matthias Bertschy <matthias.bertschy@gmail.com>

--- tests/scripts/storage-tag.sh
+++ tests/scripts/storage-tag.sh
@@ -1,5 +1,4 @@
 #/bin/bash
-# curl -s https://raw.githubusercontent.com/kubescape/helm-charts/main/charts/kubescape-operator/values.yaml -o values.yaml
-curl -s https://raw.githubusercontent.com/kubescape/helm-charts/timeseries/charts/kubescape-operator/values.yaml -o values.yaml
+curl -s https://raw.githubusercontent.com/kubescape/helm-charts/main/charts/kubescape-operator/values.yaml -o values.yaml
 yq '.storage.image.tag' < values.yaml
 rm -rf values.yaml
