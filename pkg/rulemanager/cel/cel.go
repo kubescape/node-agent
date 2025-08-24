39e4c3b653cdbe6856eb861db869e1e685b09a15
Matthias Bertschy <matthias.bertschy@gmail.com>
2025-08-24 10:11:54 +0200

    CEL evaluate native types with xcel (#621)
    * CEL evaluate native types with xcel
    
    Signed-off-by: Matthias Bertschy <matthias.bertschy@gmail.com>
    
    * CEL evaluate native types with xcel
    
    Signed-off-by: Matthias Bertschy <matthias.bertschy@gmail.com>
    
    * Adding new rules
    
    Signed-off-by: Amit Schendel <amitschendel@gmail.com>
    
    * Removing logs
    
    Signed-off-by: Amit Schendel <amitschendel@gmail.com>
    
    ---------
    
    Signed-off-by: Matthias Bertschy <matthias.bertschy@gmail.com>
    Signed-off-by: Amit Schendel <amitschendel@gmail.com>
    Co-authored-by: Amit Schendel <amitschendel@gmail.com>

--- pkg/rulemanager/cel/cel.go
+++ pkg/rulemanager/cel/cel.go
@@ -6,9 +6,20 @@ import (
 
 	"github.com/google/cel-go/cel"
 	"github.com/google/cel-go/ext"
-	"github.com/kubescape/go-logger"
-	"github.com/kubescape/go-logger/helpers"
+	tracercapabilitiestype "github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/trace/capabilities/types"
+	tracerdnstype "github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/trace/dns/types"
+	tracerexectype "github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/trace/exec/types"
+	tracernetworktype "github.com/inspektor-gadget/inspektor-gadget/pkg/gadgets/trace/network/types"
 	"github.com/kubescape/node-agent/pkg/config"
+	"github.com/kubescape/node-agent/pkg/ebpf/events"
+	tracerforktype "github.com/kubescape/node-agent/pkg/ebpf/gadgets/fork/types"
+	tracerhardlinktype "github.com/kubescape/node-agent/pkg/ebpf/gadgets/hardlink/types"
+	tracerhttptype "github.com/kubescape/node-agent/pkg/ebpf/gadgets/http/types"
+	traceriouringtype "github.com/kubescape/node-agent/pkg/ebpf/gadgets/iouring/tracer/types"
+	tracerptracetype "github.com/kubescape/node-agent/pkg/ebpf/gadgets/ptrace/tracer/types"
+	tracerrandomxtype "github.com/kubescape/node-agent/pkg/ebpf/gadgets/randomx/types"
+	tracersshtype "github.com/kubescape/node-agent/pkg/ebpf/gadgets/ssh/types"
+	tracersymlinktype "github.com/kubescape/node-agent/pkg/ebpf/gadgets/symlink/types"
 	"github.com/kubescape/node-agent/pkg/objectcache"
 	"github.com/kubescape/node-agent/pkg/rulemanager/cel/libraries/applicationprofile"
 	"github.com/kubescape/node-agent/pkg/rulemanager/cel/libraries/k8s"
@@ -16,24 +27,75 @@ import (
 	"github.com/kubescape/node-agent/pkg/rulemanager/cel/libraries/networkneighborhood"
 	"github.com/kubescape/node-agent/pkg/rulemanager/cel/libraries/parse"
 	"github.com/kubescape/node-agent/pkg/rulemanager/cel/libraries/process"
+	"github.com/kubescape/node-agent/pkg/rulemanager/types"
 	typesv1 "github.com/kubescape/node-agent/pkg/rulemanager/types/v1"
-
 	"github.com/kubescape/node-agent/pkg/utils"
+	"github.com/picatz/xcel"
 )
 
 var _ CELRuleEvaluator = (*CEL)(nil)
 
 type CEL struct {
-	env             *cel.Env
-	objectCache     objectcache.ObjectCache
-	programCache    map[string]cel.Program
-	cacheMutex      sync.RWMutex
-	evalContextPool sync.Pool
+	env          *cel.Env
+	objectCache  objectcache.ObjectCache
+	programCache map[string]cel.Program
+	cacheMutex   sync.RWMutex
 }
 
 func NewCEL(objectCache objectcache.ObjectCache, cfg config.Config) (*CEL, error) {
+	ta, tp := xcel.NewTypeAdapter(), xcel.NewTypeProvider()
+	capaObj, capaTyp := xcel.NewObject(&tracercapabilitiestype.Event{})
+	xcel.RegisterObject(ta, tp, capaObj, capaTyp, xcel.NewFields(capaObj))
+	dnsObj, dnsTyp := xcel.NewObject(&tracerdnstype.Event{})
+	xcel.RegisterObject(ta, tp, dnsObj, dnsTyp, xcel.NewFields(dnsObj))
+	execObj, execTyp := xcel.NewObject(&events.ExecEvent{})
+	xcel.RegisterObject(ta, tp, execObj, execTyp, xcel.NewFields(execObj))
+	exitObj, exitTyp := xcel.NewObject(&tracerexectype.Event{})
+	xcel.RegisterObject(ta, tp, exitObj, exitTyp, xcel.NewFields(exitObj))
+	forkObj, forkTyp := xcel.NewObject(&tracerforktype.Event{})
+	xcel.RegisterObject(ta, tp, forkObj, forkTyp, xcel.NewFields(forkObj))
+	httpObj, httpTyp := xcel.NewObject(&tracerhttptype.Event{})
+	xcel.RegisterObject(ta, tp, httpObj, httpTyp, xcel.NewFields(httpObj))
+	hardlinkObj, hardlinkTyp := xcel.NewObject(&tracerhardlinktype.Event{})
+	xcel.RegisterObject(ta, tp, hardlinkObj, hardlinkTyp, xcel.NewFields(hardlinkObj))
+	iouringObj, iouringTyp := xcel.NewObject(&traceriouringtype.Event{})
+	xcel.RegisterObject(ta, tp, iouringObj, iouringTyp, xcel.NewFields(iouringObj))
+	netObj, netTyp := xcel.NewObject(&tracernetworktype.Event{})
+	xcel.RegisterObject(ta, tp, netObj, netTyp, xcel.NewFields(netObj))
+	openObj, openTyp := xcel.NewObject(&events.OpenEvent{})
+	xcel.RegisterObject(ta, tp, openObj, openTyp, xcel.NewFields(openObj))
+	procObj, procTyp := xcel.NewObject(&events.ProcfsEvent{})
+	xcel.RegisterObject(ta, tp, procObj, procTyp, xcel.NewFields(procObj))
+	ptraceObj, ptraceTyp := xcel.NewObject(&tracerptracetype.Event{})
+	xcel.RegisterObject(ta, tp, ptraceObj, ptraceTyp, xcel.NewFields(ptraceObj))
+	randObj, randTyp := xcel.NewObject(&tracerrandomxtype.Event{})
+	xcel.RegisterObject(ta, tp, randObj, randTyp, xcel.NewFields(randObj))
+	sshObj, sshTyp := xcel.NewObject(&tracersshtype.Event{})
+	xcel.RegisterObject(ta, tp, sshObj, sshTyp, xcel.NewFields(sshObj))
+	symlinkObj, symlinkTyp := xcel.NewObject(&tracersymlinktype.Event{})
+	xcel.RegisterObject(ta, tp, symlinkObj, symlinkTyp, xcel.NewFields(symlinkObj))
+	syscallObj, syscallTyp := xcel.NewObject(&types.SyscallEvent{})
+	xcel.RegisterObject(ta, tp, syscallObj, syscallTyp, xcel.NewFields(syscallObj))
 	envOptions := []cel.EnvOption{
-		cel.Variable("event", cel.AnyType),
+		cel.Variable("event_type", cel.StringType),
+		cel.Variable(string(utils.CapabilitiesEventType), capaTyp),
+		cel.Variable(string(utils.DnsEventType), dnsTyp),
+		cel.Variable(string(utils.ExecveEventType), execTyp),
+		cel.Variable(string(utils.ExitEventType), exitTyp),
+		cel.Variable(string(utils.ForkEventType), forkTyp),
+		cel.Variable(string(utils.HTTPEventType), httpTyp),
+		cel.Variable(string(utils.HardlinkEventType), hardlinkTyp),
+		cel.Variable(string(utils.IoUringEventType), iouringTyp),
+		cel.Variable(string(utils.NetworkEventType), netTyp),
+		cel.Variable(string(utils.OpenEventType), openTyp),
+		cel.Variable(string(utils.ProcfsEventType), procTyp),
+		cel.Variable(string(utils.PtraceEventType), ptraceTyp),
+		cel.Variable(string(utils.RandomXEventType), randTyp),
+		cel.Variable(string(utils.SSHEventType), sshTyp),
+		cel.Variable(string(utils.SymlinkEventType), symlinkTyp),
+		cel.Variable(string(utils.SyscallEventType), syscallTyp),
+		cel.CustomTypeAdapter(ta),
+		cel.CustomTypeProvider(tp),
 		ext.Strings(),
 		k8s.K8s(objectCache.K8sObjectCache(), cfg),
 		applicationprofile.AP(objectCache, cfg),
@@ -51,11 +113,6 @@ func NewCEL(objectCache objectcache.ObjectCache, cfg config.Config) (*CEL, error
 		env:          env,
 		objectCache:  objectCache,
 		programCache: make(map[string]cel.Program),
-	}
-
-	// Initialize evaluation context pool to reduce map allocations
-	cel.evalContextPool.New = func() interface{} {
-		return make(map[string]any, 1)
 	}
 
 	return cel, nil
@@ -103,19 +160,9 @@ func (c *CEL) getOrCreateProgram(expression string) (cel.Program, error) {
 	return program, nil
 }
 
-func (c *CEL) EvaluateRule(event map[string]any, eventType utils.EventType, expressions []typesv1.RuleExpression) (bool, error) {
-	// Get evaluation context map from pool to reduce allocations
-	evalContext := c.evalContextPool.Get().(map[string]any)
-	defer func() {
-		// Clear and return to pool
-		clear(evalContext)
-		c.evalContextPool.Put(evalContext)
-	}()
-
-	evalContext["event"] = event
-
+func (c *CEL) EvaluateRule(event *events.EnrichedEvent, expressions []typesv1.RuleExpression) (bool, error) {
 	for _, expression := range expressions {
-		if expression.EventType != eventType {
+		if expression.EventType != event.EventType {
 			continue
 		}
 
@@ -124,9 +171,10 @@ func (c *CEL) EvaluateRule(event map[string]any, eventType utils.EventType, expr
 			return false, err
 		}
 
-		out, _, err := program.Eval(evalContext)
+		obj, _ := xcel.NewObject(event.Event)
+		out, _, err := program.Eval(map[string]any{string(event.EventType): obj, "event_type": string(event.EventType)})
 		if err != nil {
-			logger.L().Error("evaluation error", helpers.Error(err))
+			return false, err
 		}
 
 		if !out.Value().(bool) {
@@ -137,24 +185,16 @@ func (c *CEL) EvaluateRule(event map[string]any, eventType utils.EventType, expr
 	return true, nil
 }
 
-func (c *CEL) EvaluateExpression(event map[string]any, expression string) (string, error) {
+func (c *CEL) EvaluateExpression(event *events.EnrichedEvent, expression string) (string, error) {
 	program, err := c.getOrCreateProgram(expression)
 	if err != nil {
 		return "", err
 	}
 
-	// Get evaluation context map from pool to reduce allocations
-	evalContext := c.evalContextPool.Get().(map[string]any)
-	defer func() {
-		// Clear and return to pool
-		clear(evalContext)
-		c.evalContextPool.Put(evalContext)
-	}()
-
-	evalContext["event"] = event
-	out, _, err := program.Eval(evalContext)
+	obj, _ := xcel.NewObject(event.Event)
+	out, _, err := program.Eval(map[string]any{string(event.EventType): obj, "event_type": string(event.EventType)})
 	if err != nil {
-		return "", fmt.Errorf("failed to evaluate expression: %s", err)
+		return "", err
 	}
 
 	return out.Value().(string), nil
