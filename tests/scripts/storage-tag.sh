8a33ce1d23ef0bfcb4b21533af4528d236a70328
entlein <einentlein@gmail.com>
2026-02-12 16:16:03 +0100

    apparently we need to rebuild the storage go module into node-agent
    Signed-off-by: entlein <einentlein@gmail.com>

--- tests/scripts/storage-tag.sh
+++ tests/scripts/storage-tag.sh
@@ -1,4 +1,4 @@
 #/bin/bash
-curl -s https://raw.githubusercontent.com/kubescape/helm-charts/main/charts/kubescape-operator/values.yaml -o values.yaml
+curl -s https://raw.githubusercontent.com/bob/test/localtestbuild/kubescape/values.yaml -o values.yaml
 yq '.storage.image.tag' < values.yaml
 rm -rf values.yaml
