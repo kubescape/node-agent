cbcf5bd20eb477bb2ac489e21560536f957e5a4e
entlein <einentlein@gmail.com>
2026-03-11 16:37:36 +0100

    revert tests
    Signed-off-by: entlein <einentlein@gmail.com>

--- tests/images/malicious-app/malicious.go
+++ tests/images/malicious-app/malicious.go
@@ -38,116 +38,124 @@ func main() {
 }
 
 func runAllMaliciousBehaviors() error {
-	fmt.Println("Running malicious behaviors...")
-
-	// ------------------------------------------------------------------
-	// R0001 - Unexpected process launched (exec not in baseline)
-	// R1001 - Drifted process executed (binary not in base image, upperlayer=true)
-	// R0007 - Workload uses Kubernetes API unexpectedly (kubectl exec + API network)
-	// ------------------------------------------------------------------
+	// Download Kubectl binary (this should not be malicious if all is in the application profile)
 	err := downloadKubectl()
 	if err != nil {
 		fmt.Printf("Failed to download kubectl: %v\n", err)
 		return err
 	}
-	fmt.Println("[R0001/R1001/R0007] Running kubectl get secrets...")
+
+	// Run the malicious behaviors
+
+	fmt.Println("Running malicious behaviors...")
+
+	// Trigger unexpected process launch (R0001)
+	// Trigger exec binary not in base image (R1001)
+	// Trigger unexpected service account use (R0006)
+	// Trigger kubernetes client executed in container (R0007)
+	// Run Kubectl get secrets by calling kubectl binary
+	fmt.Println("Running kubectl get secrets...")
 	output, err := runKubectl("./kubectl", "get", "secrets")
 	if err != nil {
-		fmt.Printf("kubectl failed (expected): %v\n", err)
+		fmt.Printf("Failed to run kubectl: %v\n", err)
 	}
 	fmt.Print(output)
 
-	// ------------------------------------------------------------------
-	// R0006 - Unexpected service account token access
-	// ------------------------------------------------------------------
-	fmt.Println("[R0006] Opening service account token file...")
+	// Trigger unexpected Service Account Token Access (R0006)
+	// Open the service account token file
+	fmt.Println("Opening service account token file...")
 	file, err := os.Open("/run/secrets/kubernetes.io/serviceaccount/token")
 	if err != nil {
-		fmt.Printf("Failed to open SA token: %v\n", err)
+		fmt.Printf("Failed to open service account token file: %v\n", err)
 	} else {
-		file.Close()
+		// Close the file
+		err = file.Close()
+		if err != nil {
+			fmt.Printf("Failed to close service account token file: %v\n", err)
+		}
 	}
 
-	// ------------------------------------------------------------------
-	// R0002 - Files Access Anomalies in container (file under monitored path)
-	// ------------------------------------------------------------------
-	fmt.Println("[R0002] Opening malicious.txt for writing...")
+	// Trigger unexpected file access (R0002)
+	// Open a file for writing
+	fmt.Println("Opening malicious.txt for writing...")
 	file, err = os.OpenFile("malicious.txt", os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		fmt.Printf("Failed to open file: %v\n", err)
 	} else {
-		file.WriteString("This is a malicious file\n")
-		file.Close()
+		// Write to the file
+		_, err = file.WriteString("This is a malicious file\n")
+		if err != nil {
+			fmt.Printf("Failed to write to file: %v\n", err)
+		}
+		// Close the file
+		err = file.Close()
+		if err != nil {
+			fmt.Printf("Failed to close file: %v\n", err)
+		}
 	}
 
-	// ------------------------------------------------------------------
-	// R0008 - Read Environment Variables from procfs
-	// ------------------------------------------------------------------
-	fmt.Println("[R0008] Reading /proc/self/environ...")
-	envData, err := os.ReadFile("/proc/self/environ")
+	// Trigger crypto mining (R1003)
+	// Open a file for writing in the name of known_hosts
+	fmt.Println("Opening known_hosts for writing...")
+	file, err = os.OpenFile("known_hosts", os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		fmt.Printf("Failed to read /proc/self/environ: %v\n", err)
+		fmt.Printf("Failed to open file: %v\n", err)
 	} else {
-		fmt.Printf("Read %d bytes from /proc/self/environ\n", len(envData))
+		defer file.Close()
 	}
 
-	// ------------------------------------------------------------------
-	// R0010 - Unexpected Sensitive File Access (/etc/shadow)
-	// ------------------------------------------------------------------
-	fmt.Println("[R0010] Reading /etc/shadow...")
-	file, err = os.Open("/etc/shadow")
-	if err != nil {
-		fmt.Printf("Failed to open /etc/shadow (expected in distroless): %v\n", err)
-	} else {
-		file.Close()
-	}
-
-	// ------------------------------------------------------------------
-	// R0005 - DNS Anomalies in container (domain not in network neighborhood)
-	// ------------------------------------------------------------------
-	fmt.Println("[R0005] Making HTTP request to google.com...")
+	// Trigger unexpected DNS access (R0005)
+	// Make an HTTP request
+	fmt.Println("Making HTTP request to google.com...")
 	_, err = http.Get("https://www.google.com")
 	if err != nil {
 		fmt.Printf("Failed to make HTTP request: %v\n", err)
 	}
 
-	// ------------------------------------------------------------------
-	// R0003 - Syscalls Anomalies in container (unshare not in baseline)
-	// R1006 - Process tries to escape container (unshare from non-runc parent)
-	// ------------------------------------------------------------------
-	fmt.Println("[R0003/R1006] Calling SYS_UNSHARE with CLONE_NEWUSER...")
+	// Trigger unexpected system call (R0003)
+	// Trigger unshare system call (R1006)
+	// Call unshare system call
+	fmt.Println("Calling unshare system call...")
 	_, _, err = syscall.Syscall(syscall.SYS_UNSHARE, syscall.CLONE_NEWUSER, 0, 0)
 	if err != nil {
-		fmt.Printf("SYS_UNSHARE failed (expected): %v\n", err)
+		fmt.Printf("Failed to call unshare system call: %v\n", err)
 	}
 
-	// ------------------------------------------------------------------
-	// R0004 - Linux Capabilities Anomalies in container
-	// ------------------------------------------------------------------
-	fmt.Println("[R0004] Binding to privileged port 80...")
+	// Trigger unexpected capabilities (R0004)
+	// Bind to a privileged port with a socket with HTTP server than close it
+	fmt.Println("Binding to port 80...")
 	listener, err := net.Listen("tcp", ":80")
 	if err != nil {
 		fmt.Printf("Failed to bind to port 80: %v\n", err)
 	} else {
-		listener.Close()
+		// Close the listener
+		err = listener.Close()
+		if err != nil {
+			fmt.Printf("Failed to close listener: %v\n", err)
+		}
 	}
 
-	// ------------------------------------------------------------------
-	// R1000 - Process executed from malicious source (/dev/shm)
-	// ------------------------------------------------------------------
-	fmt.Println("[R1000] Creating symbolic link to ls in /dev/shm...")
+	// Symlink ls to /dev/shm and execute from there
+	fmt.Println("Creating symbolic link to ls in /dev/shm...")
+	// Remove existing file if it exists
 	os.Remove("/dev/shm/ls")
+
+	// Create symbolic link instead of copying
 	err = os.Symlink("/bin/ls", "/dev/shm/ls")
 	if err != nil {
-		fmt.Printf("Failed to create symlink in /dev/shm: %v\n", err)
+		fmt.Printf("Failed to create symbolic link in /dev/shm: %v\n", err)
 		return err
 	}
-	fmt.Println("[R1000] Executing ls from /dev/shm...")
+
+	// Try to execute the symlink first
+	fmt.Println("Executing ls from /dev/shm...")
 	cmd := exec.Command("/dev/shm/ls")
 	cmdoutput, err := cmd.CombinedOutput()
 	if err != nil {
-		fmt.Printf("Failed to execute from /dev/shm: %v\n", err)
-		// Fallback via sh
+		fmt.Printf("Failed to execute ls from /dev/shm: %v\n", err)
+
+		// If that fails, use the fallback
+		fmt.Println("Fallback: using sh -c")
 		cmd = exec.Command("sh", "-c", "cd /dev/shm && ./ls")
 		cmdoutput, err = cmd.CombinedOutput()
 		if err != nil {
@@ -158,52 +166,49 @@ func runAllMaliciousBehaviors() error {
 	} else {
 		fmt.Println(string(cmdoutput))
 	}
-	os.Remove("/dev/shm/ls")
 
-	// ------------------------------------------------------------------
-	// R1002 - Process tries to load a kernel module (SYS_INIT_MODULE)
-	// ------------------------------------------------------------------
-	fmt.Println("[R1002] Calling SYS_INIT_MODULE...")
-	_, _, err = syscall.Syscall(syscall.SYS_INIT_MODULE, 0, 0, 0)
+	// Clean up
+	err = os.Remove("/dev/shm/ls")
 	if err != nil {
-		fmt.Printf("SYS_INIT_MODULE failed (expected): %v\n", err)
+		fmt.Printf("Failed to remove symlink /dev/shm/ls: %v\n", err)
 	}
 
-	// ------------------------------------------------------------------
-	// R1004 - Process executed from mount (exec from emptyDir volume)
-	// ------------------------------------------------------------------
-	fmt.Println("[R1004] Copying kubectl to /podmount and executing...")
+	// Trigger load kernel module (R1002)
+	// Load a kernel module (call insmod system call)
+	fmt.Println("Loading kernel module...")
+	_, _, err = syscall.Syscall(syscall.SYS_INIT_MODULE, 0, 0, 0)
+	if err != nil {
+		fmt.Printf("Failed to call init_module system call: %v\n", err)
+	}
+
+	// Trigger Exec from mount (R1004)
+	// Copy the kubectl binary to /podmount
+	fmt.Println("Copying kubectl to /podmount...")
 	err = copyFile("kubectl", "/podmount/kubectl")
 	if err != nil {
 		fmt.Printf("Failed to copy kubectl to /podmount: %v\n", err)
 	} else {
+		// Call execve on the file
+		fmt.Println("Calling kubectl on /podmount/kubectl...")
+
 		out, err := runKubectl("/podmount/kubectl", "get", "secrets")
 		if err != nil {
-			fmt.Printf("kubectl on /podmount failed (expected): %v\n", err)
+			fmt.Printf("Failed to call kubectl on /podmount/kubectl: %v\n", err)
+
 		}
 		if out != "" {
 			fmt.Print(out)
 		}
 	}
 
-	// ------------------------------------------------------------------
-	// R1015 - Malicious Ptrace Usage (ptrace syscall)
-	// ------------------------------------------------------------------
-	fmt.Println("[R1015] Calling SYS_PTRACE (PTRACE_TRACEME)...")
-	_, _, err = syscall.Syscall(syscall.SYS_PTRACE, 0 /* PTRACE_TRACEME */, 0, 0)
-	if err != nil {
-		fmt.Printf("SYS_PTRACE failed (expected): %v\n", err)
-	}
-
-	// ------------------------------------------------------------------
-	// R1008 - Crypto Mining Domain Communication (DNS for known mining domain)
-	// R1009 - Crypto Mining Related Port Communication (TCP to port 45700)
-	// ------------------------------------------------------------------
-	fmt.Println("[R1008/R1009] Connecting to xmr.pool.minergate.com:45700...")
+	// Trigger crypto mining (R1007)
+	// Do a TCP connect to stratum+tcp://xmr.pool.minergate.com:45700
+	fmt.Println("Connecting to stratum+tcp://xmr.pool.minergate.com:45700...")
 	conn, err := net.Dial("tcp", "xmr.pool.minergate.com:45700")
 	if err != nil {
-		fmt.Printf("Failed to connect to mining pool: %v\n", err)
+		fmt.Printf("Failed to connect to stratum+tcp://xmr.pool.minergate.com:45700: %v\n", err)
 	} else {
+		// Close the connection
 		conn.Close()
 	}
 
@@ -247,7 +252,7 @@ func downloadKubectl() error {
 
 	fmt.Print("Downloading kubectl\n")
 
-	url := fmt.Sprintf("https://dl.k8s.io/release/v1.32.0/bin/%s/%s/kubectl", osName, arch)
+	url := fmt.Sprintf("https://dl.k8s.io/release/v1.28.4/bin/%s/%s/kubectl", osName, arch)
 
 	// Print the URL
 	fmt.Printf("Downloading kubectl from %s...\n", url)
