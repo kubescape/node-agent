12aff41bb768be520c366eb2e4cb347e5a8c9c9a
Matthias Bertschy <matthias.bertschy@gmail.com>
2026-05-28 15:25:43 +0200

    fix(otel): only overwrite env credentials with non-empty values from file in sbom-scanner

--- cmd/sbom-scanner/main.go
+++ cmd/sbom-scanner/main.go
@@ -30,8 +30,12 @@ func main() {
 	accountID := os.Getenv("ACCOUNT_ID")
 	accessKey := os.Getenv("ACCESS_KEY")
 	if creds, err := beUtils.LoadCredentialsFromFile("/etc/credentials"); err == nil {
-		accountID = creds.Account
-		accessKey = creds.AccessKey
+		if creds.Account != "" {
+			accountID = creds.Account
+		}
+		if creds.AccessKey != "" {
+			accessKey = creds.AccessKey
+		}
 	}
 
 	// Initialize OTEL providers from standard env vars (OTEL_EXPORTER_OTLP_ENDPOINT etc.).
